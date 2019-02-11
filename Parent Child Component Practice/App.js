9d6b1598102677d41e0b569d1a704b6640cc2257
krisduque <kristoffer.duque1@gmail.com>
2019-02-11 12:52:15 -0800

    Create App.js

---
+++ Parent Child Component Practice/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
+import Navbar from "./Navbar"
+
+function App(){
+    return(
+        <div>
+        <Navbar />
+        <h2>Hello world!</h2>
+        <MainContent />
+        <Footer />
+        </div>
+    )
+}
+
+export default App
