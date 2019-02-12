17e4d32b808f9362c91ac564d3e0039586788a8e
krisduque <kristoffer.duque1@gmail.com>
2019-02-12 15:31:59 -0800

    Add files via upload

---
+++ Conditional Rendering/Conditional.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Conditional(props) {
+    // condition ? statement if true : statement if false
+    return(
+        <div>
+            <h1>Loaded</h1>
+        </div>
+    )
+}
+
+export default Conditional
